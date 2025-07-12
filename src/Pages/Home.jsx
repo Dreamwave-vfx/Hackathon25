61bf50a7a7789c25795307548f2277fa6cd7e651
Ashoksuthar-str <sutharashok191@gmail.com.com>
2025-07-12 12:23:01 +0530

    navbar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div>
       <Navbar />
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Footer />
     </div>
   );
